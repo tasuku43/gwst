adf735f1a14bf06743c8f77be3f12a8c731fa4e6
tasuku43 <43329036+tasuku43@users.noreply.github.com>
2026-01-17 13:34:13 +0000

    Update Homebrew formula for v0.1.0

--- Formula/gws.rb
+++ Formula/gws.rb
@@ -3,25 +3,25 @@ class Gws < Formula
   homepage "https://github.com/tasuku43/gws"
   license "MIT"
 
-  version "0.0.0"
+  version "0.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/gws/releases/download/v0.0.0/gws_v0.0.0_macos_arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/tasuku43/gws/releases/download/v0.1.0/gws_v0.1.0_macos_arm64.tar.gz"
+      sha256 "0b6b0149ab9050bcc2f8cbb9b5cbbb3eac4a6ebcea43a1f3036b636c92fcbb72"
     else
-      url "https://github.com/tasuku43/gws/releases/download/v0.0.0/gws_v0.0.0_macos_x64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/tasuku43/gws/releases/download/v0.1.0/gws_v0.1.0_macos_x64.tar.gz"
+      sha256 "4200679e3e1ffe4440656858aaef5cec84e0d08a994bb60c2d06a24cfa9409f2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/gws/releases/download/v0.0.0/gws_v0.0.0_linux_arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/tasuku43/gws/releases/download/v0.1.0/gws_v0.1.0_linux_arm64.tar.gz"
+      sha256 "d82367681c9fbc84e2245b72490d11db4763053f0306b323edebda44269a10ec"
     else
-      url "https://github.com/tasuku43/gws/releases/download/v0.0.0/gws_v0.0.0_linux_x64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/tasuku43/gws/releases/download/v0.1.0/gws_v0.1.0_linux_x64.tar.gz"
+      sha256 "ff8ee8a7e9d5d3db6da24d85f0fc60c454b870cd846dc0d8ed164b053c5e6c54"
     end
   end
 
@@ -33,4 +33,3 @@ class Gws < Formula
     system "#{bin}/gws", "--version"
   end
 end
-
